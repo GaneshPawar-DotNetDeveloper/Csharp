a4567381d10f8ca299dc9e1ebe6a4641a14280e5
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-17 21:53:41 +0530

    Add files via upload

---
+++ Access Specifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Specifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Access Specifer
+            // -- it control of scope or acssecibility of type and type members
+            // type- class, structure, enum , delegate
+            // members- method is a type of member, feild is a type of member  
+
+
+            // type of specifers :
+            // 1. private
+            //  -  Accesiblility containing within only that type(type)  
+            // 2. protected
+            // - - within containing type and derived type  (matlab child class ko)
+            // 3. public
+            // -- access everywhere or anywhere
+            // 4. internal
+            // -- whitin only  same assembaly ( matlab vo same aseembly me access hoga ,dusre
+            //      assembly me access nahi hoga)
+            // 5.  protected internal
+            // it is  both combination of protected and internal  access specifier.
+
+            // we can dreate constructor private and protected.
+            // when we create private and protected constructor then it get not allowed to create object
+            // it use to restrict user to create object of that class.
+
+        }
+    }
+}
---
+++ Always we don't use void/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Always_we_don_t_use_void
+{        // ** void se hum value/data  return nahi kar sakate
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  firstname();
+            // lastname();
+            // ise karne se vo dega lekin alag alag dega 
+            // so vo fullname nahi dega
+            // iske liye hum called method me hum print n karake vaha se return lenge.
+            string fn = firstname();
+            string ln=lastname();
+            string fullname = ("fullname is : " + fn +" "+ln);
+            Console.WriteLine(fullname);
+
+            Console.ReadLine();
+        }
+      /*  static void firstname()
+        {
+            string fn = "ganesh";
+            Console.WriteLine(fn);
+        }
+        static void lastname()
+        {
+            string lname = "pawar";
+            Console.WriteLine(lname);
+        }*/
+
+        static string firstname()
+        {
+            string fname = "ganesh";
+            return fname;
+        }
+        static string lastname()
+        {
+            string lname = "pawar";
+            return lname;
+        }
+
+    }
+
+
+}
