89e440bade05db91cc76bf94bbc45be89b26020b
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-19 17:30:10 +0530

    Add files via upload

---
+++ Diamond problem (Inheritance)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond_problem__Inheritance_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // multiple class inheritance problem:(Diamond Problem)
+            /*
+                       - multiple class inheritance is not possible in C# 
+            - why ?
+
+                   A
+         (virtual print())
+                   /       \
+                  /         \    
+                 /           \ 
+               B :A         C:A
+       (overide print())     (overide print())
+                 \           /
+                  \         / 
+                   \       /
+                    \     / 
+                     D:B,c
+        (override print())
+
+            // isme hum yek se jyada base class inherite nahi kar sakate 
+            // isi liye C# ne ise nikal dala he (it is not support )
+
+            // multiple class inheritance is not possible 
+            // but multiple class interface i possible
+
+
+
+
+
+             */
+
+        }
+    }
+}
