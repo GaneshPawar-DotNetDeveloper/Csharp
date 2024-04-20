0a4dc74909b273785fb1bf40195b7e41287ea614
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-20 21:30:02 +0530

    Add files via upload

---
+++ Example of this keyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_this_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sc=new Employee(1,"ganesh",999999.34f);
+            sc.display();
+            Console.ReadLine();
+
+        }
+    }
+    public class Employee
+    {
+        public int id;
+        public String name;
+        public float salary;
+        public Employee(int id, String name, float salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " + salary);
+        }
+    }
+}
---
+++ Exception/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+/* 
+          Q. what is exception:
+               exception is a unexcepted output/event or any unexcepted error at run time.
+             
+            * when there is unhandeled exception:
+            *  1. code related information may get displayed to user cursor 
+            *  2. Application will get terminate.
+            
+            - so we need to handle all exception.
+          - we can handel the exception by
+              try-catch block.
+          i.try block:
+             - write all code inside the try block.
+             - try block should have atleast one catch or finally block.
+         ii. catch-block:
+             - whenever there is error in try block then catch block gets executed.
+              - we can handle error here in catch block.
+         iii. finally block:
+              - this block does not handle exception.
+              - finally block get  executed in all scenarios , when there is any error or when there 
+                 is no error.
+              - we can use finally block when we want to close any database connection or file connection
+                  or service connection.
+        
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+        }
+    }
+}
