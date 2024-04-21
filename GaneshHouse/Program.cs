a03283a95c1b59ec83fa47e3cf05755f4f340a6e
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-21 10:03:59 +0530

    Add files via upload

---
+++ GaneshHouse/Program.cs
@@ -0,0 +1,29 @@
+﻿using bedroom;
+using house;
+
+namespace GaneshHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            hall1 sw = new hall1();
+            sw.print();
+            rooms r= new rooms();
+
+
+        }
+    }
+}
+// hum exe or dll ka live ex. dekhate he 
+/*
+    company me humare group hote he mean yek project pe kam karane  tester,developer ,
+    diployer hote he ,
+     to jab hum develope ka kam karate he to o dll file hoti he, use hum age deployer ko
+     share kaarate he ,kyuki usake pass main method vali exe file rahati he ,usaka hume 
+     access nahi hota .
+--- when we add project in class laibrary == .dll file
+    when we add project in console application== .dll file also .exe file
+ 
+ 
+ */
