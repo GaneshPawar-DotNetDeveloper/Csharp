befc46e5195df3f8a7715d460b588ddff406549a
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-23 12:39:03 +0530

    Add files via upload

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+/*  
+ // ploymorphism matlab hume methods same rakhana he lekin uska use alag -alag purpose ke liye karana he
+          single thing having multiple form that is polymorphism.
+           mean zendubam= yek bam tin kam . matlab zendubam is polymorphism.
+        konsi bhi chise ke multiple fform or multiple purpose he.
+  
+             2 types of polymorphism:
+   1. static polymorphism (compile time polymorphism )
+
+            -- method overloading
+            -- operator overloading
+   2. dynamic polymorphism (runtime polymorphism)
+            -- method overriding
+
+
+            i. method overloading:
+             a. number of parameters
+             b. types of parameters
+             c. order of parameters
+             d. kinds of parameters
+ 
+            a. numbers of parameters:
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+        }
+    }
+}
