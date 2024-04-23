e2bd63dcf04162a11998795659f5b2cbdbb8e9eb
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-23 12:38:16 +0530

    Add files via upload

---
+++ Operator overloading/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 140;
+            int k = i + j; 
+            // + is a operator && i,j is a operands 
+            Console.WriteLine(k);
+
+            string s = "ganesh";
+            string l = "pawar";
+            string r = s+l;
+            // yaha humne in-built oprand ke sath operator use kiya he.
+            // iske vajah se hum directly usi type me kar sakate he 
+
+            Console.WriteLine(r);
+            // but we have custom type/complex type/class
+
+            calculator c1= new calculator() { number1=10, number2=12};
+            calculator c2= new calculator() { number1=20, number2 = 12 };
+            calculator c3 = c1 + c2; // +-> operator,c1,c2->operands
+            // here it give compile time error because + operator can't understand how to add calculator
+            // variables, because in class calculators there as multiple feilds,members.
+            // so for these reason/ purpose we can overload + operator.
+            
+           Console.WriteLine(c3.number1);
+            Console.WriteLine(c3.number2);
+            // withot create method we can get output 
+
+            /* calculator c4 = new calculator();
+             c4.number1 = c1.number1 + c2.number1;
+             Console.WriteLine(c4.number1);*/
+
+            // but we have 50 object and suddenly we want to add or change another value then 
+            // we go for create another 50 objects
+            // so for that we can create opertor overload.
+            /* calculator c6 = new calculator() { number2 = 1034 };
+             calculator c7 = new calculator() { number2 = 133 };
+             calculator c8 = c6 + c7;*/
+            // Console.WriteLine(c8.number2);
+            Console.ReadLine();
+        }
+    }
+    public  class calculator
+    {
+        public int number1;
+        public int number2;
+        public static calculator operator + (calculator c1,calculator c2)
+        { 
+            calculator c= new calculator();
+             c.number1=c1.number1 + c2.number1;
+             c.number2= c1.number2 + c2.number2;
+            return c;
+        }
+
+    }
+}
---
+++ params keyword/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parems_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // params keyword we can only use with array.
+            // for calling a printsum method for that we create a array
+            int[] numb = new int[] { 1, 2, 3, 4, 5, 6, };
+            printsum(numb);
+
+           Console.WriteLine("--------------------");
+
+            // we can direct put object in method it also get
+            printsum(new int[] { 12343,345654,3,3,4,3});
+            Console.WriteLine("-----------------");
+            int[] numb2 = null;
+            printsum (numb2);
+            Console.WriteLine("----------------");
+            printsum( null );
+            Console.WriteLine("-------------");
+            printsum(new int[] { -12, 123, 1233, -23, -443 });
+            Console.WriteLine("-----------");
+
+            // in above all example we put value in method like printsum(new int[] { 12343,345654,3,3,4,3});
+             // but we want put value like comma separeted (printsum(11,23,4,553)) and for null
+             // we put empty method (printsum())
+             // so for that we can use params keyword
+             // params keyword we can use with method parameter
+             // so we can put like that:
+             printsum ();
+            Console.WriteLine("with param empty method");
+            printsum(12, 234, 234, 654,23);
+            Console.WriteLine("direct put comma separeted values");
+            // params keyword we can pass with only array paramerter
+            // there are two benifits of params :
+            // 1. we can put comma separeted value as a input
+            // 2. input araay will bw optional. 
+
+
+            Console.ReadLine(); 
+        }
+        static void printsum( params int[] numbers) {
+
+            // when we have multiple parameters then params keyword should be apply on last parameter
+            if (numbers != null && numbers.Length > 0)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum = sum + numbers[i];
+                }
+                    Console.WriteLine("Addition : {0}", sum );
+                
+            }
+            else
+            {
+                Console.WriteLine("empty array");
+            } }
+    }
+}
