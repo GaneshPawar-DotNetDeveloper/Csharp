38686204aa34cc6263246d657aaf04111746b9b8
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-24 15:42:17 +0530

    Add files via upload

---
+++ Property In C#/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_In_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student s=new student();
+          //  s.id = 1;    // its give set method
+            Console.WriteLine(s.id); // its give get method
+            // but here user give invalid value so for that we write a condition in set method
+            s.id = -1;
+
+
+            student s2=new student();
+            s2.name = "";
+            Console.WriteLine(s2.name);
+
+            student s3= new student();
+            s3.age = -10;
+            Console.WriteLine(s3.age);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+    public class student
+    {
+         int _id;
+       string _name;
+        int _age;
+
+        // here we give filed as _id because we understand the difference between feilds and property.
+       
+        public int id
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("you enter invalid id\n");
+                }
+                else
+                {
+                    this._id = value;
+                }
+            }
+            get
+            {
+                return _id; 
+            }
+        }
+        public string name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(_name) == true)
+                {
+                    Console.WriteLine("pelase enter the name");
+                }
+                else
+                {
+                    _name = value;
+
+                }
+            }
+            get
+            {
+                return _name;
+            }
+        }
+        public int age
+        {
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("that is invalid age");
+
+                }
+                else
+                {
+                    this._age = value;
+                }
+                
+            }
+            get
+            {
+                return _age;
+            }
+        }
+
+    }
+
+}
---
+++ Questions&&Answer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions__Answer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. hume yek method return karana he jo ki array return kare
+            //  GetNotnetstudent(); // we can not do that becuse we have a multiple value 
+            // so for we ceate a variable and iterate it with hepl of for loop
+            /* string[] names = GetNotnetstudent();
+             // string[] namesss = names.split(new char[] {','});
+              for (int i = 0;  i < names.Length;i++)
+              {
+                  Console.WriteLine(names[i]);
+              }*/
+
+            // ref recall:
+            string cname = "dotnet";
+            updatecourse(ref  cname);
+            Console.WriteLine(cname);
+            Console.ReadLine(); 
+
+        }
+        /*static string[] GetNotnetstudent()
+        {
+            string[] names = new string[] { "ganesh", "suraj", "raj", "uday", "padam" }; // it not need 
+            // we assume it come from database
+            return names;
+        }*/
+         static void updatecourse(ref string coursenames)
+        {
+            bool hascoursechange = true;    // from database
+            if (hascoursechange)
+            {
+                coursenames = "python";
+            }
+
+        }
+    }
+}
