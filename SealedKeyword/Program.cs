f5b1509088eba86ab0039244d3a3f7d84a823377
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-24 15:43:10 +0530

    Add files via upload

---
+++ SealedKeyword/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedKeyword
+{
+    // Use Of Sealed Keyword
+    // To Restrict a Class For Inheritance
+    // Sealed Keyword We Can Use With Class And Method.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    // Sealed Class :
+    // No One Can Inherit This Class 
+    // Rest Of Things Are Same.
+ /* public  class A { }
+  public sealed  class B : A{ }  // Class B can not inherited because of sealed
+  // public class C : B { }  */ 
+
+
+    // sealed method example:
+   public class A
+    {
+      public virtual void print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    public class B :A
+    {
+        public  sealed override void print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    // here we wnat to not allowed overide to class c method
+    // so for that we can declare method B as sealed.
+    // but we can write sealed for class A method then it give error like you can not get sealed for print 
+    // A method beacause it can not override.
+
+    // So Sealed Method Cannot Be Override In Derived  Class
+    // Sealed Keyword We Can Use Only With Override Method.
+
+
+    public class C: B
+    {
+       /* public override void print()
+        {
+            base.print();
+        }*/
+
+    }
+
+}
---
+++ Split - Comma Separated/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split___Comma_Separated
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("please enter your education in comma separareted value ");
+             string value=Console.ReadLine();
+           //  Console.WriteLine(value);
+             string[] education=value.Split(new char[] { '-' }); // yaha hum konsi bhi char value de sakate he
+            // Console.WriteLine(education);
+             for(int i = 0; i < education.Length; i++)
+             {
+                 Console.WriteLine(education[i]);
+             }*/
+            // reverse sentance with split method
+            Console.WriteLine("please enter your sentance");
+          string value=Console.ReadLine();
+          /*int value=int.Parse(Console.ReadLine());
+            int[] words=value.Split(new char[] { ',', '.', ':', });*/
+            string[] words = value.Split(new char []{',','.',':',});
+            for (int i = words.Length-1;i>= 0;i--)
+            {
+                Console.WriteLine(words[i] +"  ");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
