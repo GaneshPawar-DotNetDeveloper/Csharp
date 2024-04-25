ca0e28a94e656aff1623d48ae3d647dc5dd136f8
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-25 18:02:39 +0530

    Add files via upload

---
+++ VoidMethodWithReturnType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodWithReturnType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  print("null", "pawar");
+            //print("ganesh","pawar");
+            print("ganesh",null);
+
+            Console.ReadLine();
+        }
+static void print(string s,string s1)
+        {
+            if (string.IsNullOrEmpty(s)){
+                return;
+            }
+            Console.WriteLine($"you entered : {s} {s1}");
+        }
+
+
+    }
+}
