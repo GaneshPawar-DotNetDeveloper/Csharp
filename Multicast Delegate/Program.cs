b7fabb9cdf8972e4156343640cee042d43ae71a5
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-27 17:12:45 +0530

    multicast delegate

---
+++ Multicast Delegate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Multicast_Delegate
+{
+
+
+    public delegate void printdelegate();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            printdelegate del = printA;
+         
+            printdelegate del2 = printB;
+          
+            printdelegate del3 = printC;
+
+            printdelegate d= del + del2+ del3;
+
+            d();
+            printdelegate d2= d- del;
+            d2();
+
+
+
+            Console.ReadLine();
+
+        }
+
+        static void printA()
+        {
+            Console.WriteLine("A");
+        }
+        static void printB()
+        {
+            Console.WriteLine("B");
+
+        }
+        static void printC()
+        {
+            Console.WriteLine("C");
+        }
+    }
+}
---
+++ multiple delegate with Return type/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_delegate_with_Return_type
+{
+
+    public delegate string printDelagateMethod();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printDelagateMethod sc = printA;
+            printDelagateMethod sc1 = printB;
+            printDelagateMethod sc2 = printC;
+
+            printDelagateMethod s = sc + sc1 + sc2;
+
+            /*  string result = s();
+              Console.WriteLine(s);
+  */
+            // so here problem is when we use multicast delegate woth return type that time it can return last value
+            // so for that we can use getInvoctionList().
+            delegate[] delegates = s.GetInvocationList();
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                var sol = ((printDelagateMethod)delegates[i]).Invoke(); // corrected the typo here
+                Console.WriteLine(sol);
+            }
+
+
+            Console.ReadLine();
+        }
+        static string printA()
+        {
+            return "A";
+        }
+        static string printB()
+        {
+
+            return "B";
+        } static string printC()
+        {
+            return "C";
+        }
+    }
+}
