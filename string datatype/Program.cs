c9a5c979a7277d399c5b8b63208dba84cfaf163c
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-28 23:14:50 +0530

    Add files via upload

---
+++ string datatype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_datatype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "ganesh pawar";
+            int b = 12;
+            float c = 14.4f;
+            double d = 12334444;
+            char e = 'A';
+            Boolean f= true;
+            byte g = 233;
+
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+    }
+}
