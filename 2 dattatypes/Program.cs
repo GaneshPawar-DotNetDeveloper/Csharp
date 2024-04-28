c3e5d93f0b38323718b70deba2018cf74c74585a
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-28 23:10:28 +0530

    Add files via upload

---
+++ 2 dattatypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_dattatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int c= 24;
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
