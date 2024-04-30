134c61b76e599fe3112485f73186e9e9b17b04a4
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-30 20:29:58 +0530

    Add files via upload

---
+++ 9_looping statements/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_looping_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Looping Statements 
+
+            //for (int i = 1; i <= 5;i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //    if (i == 3)
+            //    {
+            //      break;
+            //    }
+
+               
+            //}
+            // break statement
+            //--- its terminate switching and looping statement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
