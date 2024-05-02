49c2daa9257fa0cb9428a942fbd2d7a5e588336e
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-02 17:23:29 +0530

    Add files via upload

---
+++ basicOfC#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicOfC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(3 + 3);
+
+            Console.WriteLine("hello ganesh");
+            Console.ReadLine();
+
+            Console.WriteLine(2 + 3);
+            Console.ReadLine();
+
+
+            Console.WriteLine("x + y + z");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class and object/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student(); // this is the object/ instance of student class
+            s.id = 1;
+            s.name = "ganesh";
+            s.age = 24;
+            s.age = 12;
+            Console.WriteLine(s);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+    class student  // student is type of class type
+    {
+        public int id;
+        public string name;
+        public int age;// these are the fields/members of the class
+    }
+}
+
