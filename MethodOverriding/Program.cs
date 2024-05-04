5fd95d2c4387108b49b4c73e1bedcb61125aeb33
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-04 21:37:24 +0530

    Add files via upload

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    
+    public class parent
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("this is parent class method");
+        }
+    }
+    public class child : parent
+    {
+        public  override void print()
+        {
+            Console.WriteLine("this is child class method");
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+parent p= new parent();
+
+p.print();
+            parent c= new child();
+            c.print();
+
+
+
+Console.ReadLine();
+        }
+    }
+}
