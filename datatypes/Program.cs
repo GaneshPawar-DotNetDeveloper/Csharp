204af6d4dd947f57061127b87ce41cc06a7d1dde
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-04 21:36:50 +0530

    Add files via upload

--- datatypes/Program.cs
+++ datatypes/Program.cs
@@ -1,2 +1,165 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            int b = 1244;
+            byte c = 23;
+            short d = 234;
+            int e = 122344445;
+            long f = 1234567890;
+            float g = 12.6f;
+            double h = 122667.234556767798654333;
+            decimal i = 122.1234567890m;
+            char j = 'A';
+            string k = "GANESH PAWAR";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            // Console.WriteLine(A);
+            // Console.WriteLine(A);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+            // Console.ReadLine();
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(sizeof(char));
+            Console.WriteLine(sizeof(short));
+            Console.WriteLine(sizeof(byte));
+            Console.WriteLine(sizeof(decimal));
+
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(a);
+
+            float p = 123.23F;
+            Console.WriteLine(p);
+
+
+            double q = 123.34578765;
+            Console.WriteLine(q);
+
+            decimal r = 12344.345663234566677m;
+            Console.WriteLine(r);
+
+            // for finding time 
+            DateTime dob = DateTime.Now;
+            Console.WriteLine(dob);
+
+            // for string value
+
+            string name = "ganesh" +
+                "pawar ";
+            Console.WriteLine(name);
+
+            string print = "dot net framework";
+            Console.WriteLine(print);
+
+            //  Console.ReadLine();
+
+            int z = 234;
+            Console.WriteLine(b);
+            //  Console.ReadLine();
+
+            // Datatypes
+            // 1. boolean datatype 
+
+            bool b1 = true;
+            Console.WriteLine(b1);
+            Console.WriteLine(sizeof(bool));
+
+            //2. integral datatype:
+            // a. byte
+            // b. short
+            // c. int
+            // d. long
+            //  byte / short / int / long values = 2;
+            // it get bydefault value as a int 
+            int value = 2;
+            Console.WriteLine(sizeof(short));
+            Console.WriteLine(sizeof(byte));
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(sizeof(long));
+            Console.WriteLine(value);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            Console.WriteLine($"{a}:{b},{c}@ {d}#{value}' {short.MinValue}:{e}");
+            Console.WriteLine($"{a}:{short.MaxValue}:{short.MinValue}:{int.MinValue}");
+            Console.WriteLine($"{d}:{float.NaN}:{float.MinValue}{d}{f}{e}{g}{h},string as a get value,(dwhfiwdw)");
+            //  Console.ReadLine();
+            // 3 .floating type
+
+            //  a.float
+            //  b.double
+            //  c. decimal
+            // it get bydefault value as a double 
+
+            float ie = 10.55f;
+            Console.WriteLine(ie);
+
+            double pq = 12234.3453;
+            Console.WriteLine(pq);
+
+            decimal jq = 1234.123m;
+            Console.WriteLine(jq);
+            //Console.ReadLine();
+
+            // string type 
+            // 1. char
+            // 2. string
+            char division = 'A';
+            Console.WriteLine(division);
+            string ab = "ganesh pawar";
+            Console.WriteLine(ab);
+            string www = "health is wealth";
+            Console.WriteLine(www);
+            char number = '+';
+            Console.WriteLine(number);
+            char vv = '*';
+            Console.WriteLine(vv);
+            char DD = ';';
+            Console.WriteLine(DD);
+            Console.WriteLine(char.MaxValue);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ method parameters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_parameters
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a =10;
+            printA(a);
+            Console.WriteLine(a);
+            printB(ref a);
+            Console.WriteLine(a);
+            int bc;
+            printC(out bc);
+            Console.WriteLine(bc);
+            Console.ReadLine();     
+        }
+        //1. pass by value 
+        static void printA(  int  ba)
+        {
+            ba = 100;
+        }
+        //2. pass by ref 
+      static void printB (ref int b)
+        {
+           // b = 10000;
+        }
+        //3. out keyword
+        static void printC( out int c)
+        {
+            c = 2344;
+        }
+    }
+}
