7da7e41a8752921aa43536cdc1e4ecc6ef56bb50
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-11 09:16:40 +0530

    Add files via upload

---
+++ simple class laibrary project/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_class_laibrary_project
+{
+    public class Class1
+    {
+        // class liabrary me main method nahi hoti, isaka matlab vo self executed nahi he.
+
+    }
+}
---
+++ simple class laibrary project/house.cs
@@ -0,0 +1,19 @@
+﻿using System;  
+namespace house {
+    public class hall1 { 
+
+        public void prints()
+        {
+            Console.WriteLine("hall1 is open");
+        }
+    }
+    public class hall2
+    {
+        public void print()
+        {
+            Console.WriteLine("Hall2 is open");
+
+        }
+    }
+
+}
