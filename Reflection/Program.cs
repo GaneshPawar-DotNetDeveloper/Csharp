eafc78d23a5149650a6a8052afa1b80c200baa55
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-11 09:15:07 +0530

    Add files via upload

---
+++ Reflection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* student student = new student();
+             student.print("ganesh", "pawar");*/
+
+            // so when  student type is not available at compile time then it 
+            // not pissible to access that method.
+            // for that we can use reflection 
+            // refection all related classes are present in system.reflection namespace
+
+        Assembly  as  =    Assembly.GetExecutingAssembly(); // it returns assembly object
+
+            Console.ReadLine(); 
+        }
+    }
+   /* // this is the early binding:
+
+    public class student
+    {
+        public void print (string a , string b)
+        {
+            Console.WriteLine($"{a}  {b}");
+        }
+    }*/
+
+    // but i want this type can available at runtime 
+}
