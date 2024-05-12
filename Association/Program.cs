d08da17811e32753cc23bff20734dd3a87d9a48b
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-12 23:18:56 +0530

    Add files via upload

---
+++ Association/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            department s= new department();
+            employee sw= new employee(d);
+
+            Console.ReadLine();
+        }
+    }
+    public class department
+    {
+
+    }
+    public class employee {
+    public department dept { get; set; } // agreegation
+    
+        public employee(department d) {  // composition
+        
+       dept = d;
+        }
+
+    }
+}
