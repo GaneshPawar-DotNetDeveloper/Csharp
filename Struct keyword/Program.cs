a83aabf7883f46a6518fab763f37379a32f2707d
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-14 01:27:57 +0530

    Add files via upload

---
+++ Struct keyword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("99% we can't use struct bcs it as  \n some limitation " +
+                "like:\n A. only value type \n B. bydefault sealed \n C. not possible parameterless cons.\n D. can't use oop's ");
+       
+        Console.ReadLine();
+        }
+    }
+}
