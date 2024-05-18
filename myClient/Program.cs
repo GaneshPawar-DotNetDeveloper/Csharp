7d90bddc13b18ab8653a20080157b33a528ae934
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-18 21:00:28 +0530

    Add files via upload

---
+++ myClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myLiabraryproject;
+namespace myClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 sc=new Class1();
+            sc.printmethod();
+
+
+
+            // but here i want to call myclient project-clientclass-printMethod call in myliabrary project which
+            // as dll project file- class1 me call karana he to kaise kare?
+            //--> by reference karane jayenge to vo hume circular dependency ka error dega.
+            // so for that we use delegate.
+            // so class1 me hum yek delegate add karenge.
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ myLiabraryproject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLiabraryproject
+{
+
+
+    public delegate void printdelegate();
+    public class Class1
+    {
+
+
+        public void printmethod(printdelegate a )
+        {
+            a();
+        }
+    }
+}
